039961c250cb34d9b76bded366200efc221644b9
Jacob Senko <37751054+Okub1@users.noreply.github.com>
2022-08-19 13:38:00 +0200

    first website

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<head><title>First website!</title></head>');
+    res.write('<body><h1>Welcome to my first website in node.js!</h1></body>');
+    res.write('</html>');
 });
 
+// visit localhost:3000 in browser, to see my first website! :)
 server.listen(3000);
