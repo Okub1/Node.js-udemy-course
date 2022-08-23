847b7aa90a7f8b1c26a424ede49802105be8a574
Jacob Senko <37751054+Okub1@users.noreply.github.com>
2022-08-23 17:37:49 +0200

    added html files

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     console.log('in another middleware');
-    res.send('<h1>Hello from express!</h1><br><a href="/admin/add-product"><button>Add product</button></a>');
+    res.sendFile(path.join(__dirname, '..','views', 'shop.html'));
 });
 
 module.exports = router;
