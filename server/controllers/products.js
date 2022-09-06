9b8eebd5a01e2131b30202e043bc334e3a08b922
Jacob Senko <37751054+Okub1@users.noreply.github.com>
2022-09-06 11:47:52 +0200

    changed backend to use cors

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,5 +1,15 @@
 const Product = require('../models/product');
 
+// push dummy products to database
+let product = new Product(1, 'Book', 'lorem ipsum aslkdjaljfl', '/img/products/1.jpg');
+product.save();
+product = new Product(2, 'Glass');
+product.save();
+product = new Product(3, 'Phone');
+product.save();
+product = new Product(4, 'Chewing gum');
+product.save();
+
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
         pageTitle: 'Add product', 
@@ -8,7 +18,7 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const product = new Product(req.body.id, req.body.title);
     product.save();
     res.redirect('/');
 };
@@ -16,10 +26,14 @@ exports.postAddProduct = (req, res, next) => {
 // returns all products 
 exports.getProducts = (req, res, next) => {
     const products = Product.fetchAll();
-    res.json({products: products});
+    res.json(products);
 }
 
+// returns specific product
 exports.getProduct = (req, res, next) => {
     const productId = req.params.productId;
-    res.json({productId: productId});
+    Product.findById(productId, product => {
+        console.log(product, 'hi mom!');
+        res.json(product);
+    });
 };
